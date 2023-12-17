8ec94d8eeabc93533ac2a38f6c17aaeb6d9cf031
Attar Akram Abdillah <127123155+attarakram121140013@users.noreply.github.com>
2023-12-18 05:58:28 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,12 @@
 <?php
 
 function koneksiDatabase() {
-    $koneksi = mysqli_connect("localhost", "id21685284_attar", "At-12-11-40-013", "id21685284_seri_animasi");
+    $host = "localhost";
+    $username = "id21685284_attar";
+    $password = "At-12-11-40-013";
+    $database = "id21685284_seri_animasi";
+    
+    $koneksi = mysqli_connect($host, $username, $password, $database);
 
     if (!$koneksi) {
         die("Koneksi gagal: " . mysqli_connect_error());
